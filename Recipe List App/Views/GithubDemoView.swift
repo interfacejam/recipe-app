2dce55bcfd7872aa63b2cb2928abe0ecd32f88d0
Interfacejam <klauzz.design@gmail.com>
2022-05-07 19:52:32 +0100

    Add SwiftUI View

---
+++ Recipe List App/Views/GithubDemoView.swift
@@ -0,0 +1,20 @@
+//
+//  GithubDemoView.swift
+//  Recipe List App
+//
+//  Created by Claudio Di Liberto on 07/05/2022.
+//
+
+import SwiftUI
+
+struct GithubDemoView: View {
+    var body: some View {
+        Text("Hello guys!")
+    }
+}
+
+struct GithubDemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        GithubDemoView()
+    }
+}
